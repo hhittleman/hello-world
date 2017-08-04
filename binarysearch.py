2940aed18473b2ad136cc761fa0e29d3b6107f14
hhittleman <hannah.hittleman@gmail.com>
2017-08-04 11:11:46 -0700

    Add files via upload

---
+++ binarysearch.py
@@ -0,0 +1,33 @@
+import csv
+import string
+
+# Open the CSV File and read it in.
+f = open('countries.csv')
+data = f.read()
+
+# Split the data into an array of countries.
+countries = data.split('\n')
+
+length = len(countries)
+print(countries[1])
+print(countries)
+
+italy == num
+start == 0
+length == n
+end == n-1
+num == true
+while num == true:
+    end - start == newlength
+    newlength/2 == mid
+    end-mid == pivot index
+    for i in range (n):
+        if num < pivot index
+            end == pivot index
+            return;
+        if num > pivot index
+            start == pivot index
+            return:
+        if num == pivot point
+            num == false
+            break;
